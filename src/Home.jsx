7e3e4696fce1551e164d40e7e2f0ef98783a5250
Kaori Tsushima <kaoritsushima0107@gmail.com>
2023-09-14 18:52:06 +0100

    completed home

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import "./Home.css";
 // import ReactDOM from "react-dom";
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-// import { faLinkedin } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
 export default function Home() {
   return (
@@ -16,12 +18,38 @@ export default function Home() {
       </div>
       <div className="greeting ">
         <h4>Hello,</h4>
-        <h1>I'm Kaori Tsushima</h1>
+        <h1>
+          I'm <strong>Kaori Tsushima</strong>
+        </h1>
         <h2>React Front-end Developer</h2>
         <h3>Based in Glasgow, UK</h3>
-        {/* const element = <FontAwesomeIcon icon={faEnvelope} />
-        ReactDOM.render(element, document.body) */}
-        {/* <FontAwesomeIcon icon={faLinkedin} /> */}
+
+        <div className="d-flex gap-3 justify-content-center mt-4">
+          <a
+            href="https://www.linkedin.com/in/kaori-tsushima/"
+            target="_blank"
+            rel="noreferrer"
+            className="icon-color"
+          >
+            <FontAwesomeIcon icon={faLinkedin} size="2x" />
+          </a>
+          <a
+            href="https://github.com/KaoriTsushima"
+            target="_blank"
+            rel="noreferrer"
+            className="icon-color"
+          >
+            <FontAwesomeIcon icon={faGithub} size="2x" />
+          </a>
+          <a
+            href="mailto:kaoritsushima0107@gmail.com"
+            target="_blank"
+            rel="noreferrer"
+            className="icon-color"
+          >
+            <FontAwesomeIcon icon={faEnvelope} size="2x" />
+          </a>
+        </div>
       </div>
     </div>
   );
