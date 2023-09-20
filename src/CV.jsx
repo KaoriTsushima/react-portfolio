7d71be6c0dd1bfbf2cb41b34aa7f24b6f8993c80
Kaori Tsushima <kaoritsushima0107@gmail.com>
2023-09-20 10:44:51 +0100

    trying to attach pdf file

--- src/CV.jsx
+++ src/CV.jsx
@@ -1,4 +1,19 @@
 import React from "react";
+import "./CV.css";
+import { Document } from "react-pdf";
 export default function CV() {
-  return <p>CV</p>;
+  return (
+    <div className="cv pt-3 pb-4">
+      <h1>CV</h1>
+      {/* <a
+        class="btn btn-primary"
+        role="button"
+        target="_blank"
+        href=".\Resume-Kaori.pdf"
+      >
+        <Document file="./Resume-Kaori.pdf" />
+        Download CV
+      </a> */}
+    </div>
+  );
 }
