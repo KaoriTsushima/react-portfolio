6efc154bb522a14ec685b31f98bb72532d5aae82
Kaori Tsushima <kaoritsushima0107@gmail.com>
2023-09-25 16:45:16 +0100

    added work experience data

---
+++ src/workExperienceData.js
@@ -0,0 +1,48 @@
+export const workExperienceData = [
+  {
+    company: "Pole to Win",
+    role: "Tester",
+    startEnd: "Sep 2019 - present",
+    location: "Glasgow, UK",
+    duty: [
+      "Report gaming bugs using Jira",
+      "Manage daily tasks and deadline as a team",
+    ],
+  },
+  {
+    company: "Glasgow Royal Infirmary",
+    role: "Healthcare Support Worker",
+    startEnd: "Nov 2021 - Mar 2023",
+    location: "Glasgow, UK",
+    duty: ["Support patients' daily life", "Assist nursing work"],
+  },
+  {
+    company: "HC One",
+    role: "Care Assisstant",
+    startEnd: "Mar 2021 - Sep 2021",
+    location: "Glasgow, UK",
+    duty: [
+      "Support residents' daily life",
+      "Improve residents' quality of life",
+    ],
+  },
+  {
+    company: "Higashiyamato Hospital",
+    role: "Certified Nurse",
+    startEnd: "Dec 2014 - Mar 2019",
+    location: "Tokyo, Japan",
+    duty: [
+      "Analyse and manage prevalence of pressure ulcers",
+      "Educate staff",
+      "Treat wounds as a lead nurse",
+      "Run community of pressure ulcers",
+    ],
+  },
+  {
+    company: "Tokyo Metropolitan Tama Medical Center",
+    role: "Staff Nurse",
+    startEnd: "Apr 2010 - Nov 2014",
+    location: "Tokyo, Japan",
+    duty: ["Provide nursing skills to patients"],
+  },
+];
