7f7467ec34373052cee75e9b6a0010c3f0f20466
Kaori Tsushima <kaoritsushima0107@gmail.com>
2023-09-26 15:15:42 +0100

    added work experience

--- src/WorkExperience.jsx
+++ src/WorkExperience.jsx
@@ -1,4 +1,21 @@
 import React from "react";
+import { workData } from "./workData.js";
+
 export default function WorkExperience() {
-  return <p>work-experience-contents</p>;
+  return workData.map((data) => (
+    <div className="work-details">
+      <h3>{data.company}</h3>
+      <h4>{data.role}</h4>
+      <p>
+        {data.startEnd}
+        {""}
+        {data.location}
+      </p>
+      <ul>
+        {data.duties.map(function (duty) {
+          return <li>{duty}</li>;
+        })}
+      </ul>
+    </div>
+  ));
 }
--- src/workData.js
+++ src/workData.js
@@ -1,10 +1,10 @@
-export const workExperienceData = [
+export const workData = [
   {
     company: "Pole to Win",
     role: "Tester",
     startEnd: "Sep 2019 - present",
     location: "Glasgow, UK",
-    duty: [
+    duties: [
       "Report gaming bugs using Jira",
       "Manage daily tasks and deadline as a team",
     ],
@@ -14,14 +14,14 @@ export const workExperienceData = [
     role: "Healthcare Support Worker",
     startEnd: "Nov 2021 - Mar 2023",
     location: "Glasgow, UK",
-    duty: ["Support patients' daily life", "Assist nursing work"],
+    duties: ["Support patients' daily life", "Assist nursing work"],
   },
   {
     company: "HC One",
     role: "Care Assisstant",
     startEnd: "Mar 2021 - Sep 2021",
     location: "Glasgow, UK",
-    duty: [
+    duties: [
       "Support residents' daily life",
       "Improve residents' quality of life",
     ],
@@ -31,7 +31,7 @@ export const workExperienceData = [
     role: "Certified Nurse",
     startEnd: "Dec 2014 - Mar 2019",
     location: "Tokyo, Japan",
-    duty: [
+    duties: [
       "Analyse and manage prevalence of pressure ulcers",
       "Educate staff",
       "Treat wounds as a lead nurse",
@@ -43,6 +43,6 @@ export const workExperienceData = [
     role: "Staff Nurse",
     startEnd: "Apr 2010 - Nov 2014",
     location: "Tokyo, Japan",
-    duty: ["Provide nursing skills to patients"],
+    duties: ["Provide nursing skills to patients"],
   },
 ];
